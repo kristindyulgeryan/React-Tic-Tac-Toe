d5e6b7daf249082a40ff42ede48b38c435a1fe01
Kristin <kristindyulgeryan@abv.bg>
2025-02-21 14:43:18 +0200

    Add edit buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ function App() {
     Players
 <ol id="players">
 <li>
+  <span className="player">
   <span className="player-name">Player 1</span>
   <span className="player-symbol">X</span>
+  </span>
+  <button>Edit</button>
 </li>
 <li>
+  <span className="player">
   <span className="player-name">Player 2</span>
   <span className="player-symbol">O</span>
+  </span>
+  <button>Edit</button>
 </li>
 
 </ol>
