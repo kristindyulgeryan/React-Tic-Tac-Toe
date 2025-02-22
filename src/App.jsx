0b482b19554a87e847ed25eb4b060dde3d1381e9
Kristin <kristindyulgeryan@abv.bg>
2025-02-22 20:18:03 +0200

    Set up user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import Player from "./components/Player.jsx";
 
 function App() {
-  
-  return <main>
-    <div id="game-container">
-    PLAYERS
-<ol id="players">
-<Player name="Player 1" symbol='X'/>
-<Player name="Player 2" symbol='O'/>
-</ol>
-
-
-
-    GAME BOARD
-    </div>
-  
-  LOG
-  </main>;
+  return (
+    <main>
+      <div id="game-container">
+        PLAYERS
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
