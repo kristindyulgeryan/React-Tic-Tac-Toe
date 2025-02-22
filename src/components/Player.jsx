c6f304d301e4db30b4f0bf2c922f2dea615edbe4
Kristin <kristindyulgeryan@abv.bg>
2025-02-22 18:32:43 +0200

    Add updating state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,26 @@
-import { use } from "react"
-import { useState } from "react"
+import { useState } from "react";
 
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
 
-export default function Player({name, symbol}){
-const [isEditing, setIsEditing] = useState(false) ;
+  function handleEditclick() {
+    setIsEditing((editing) => !editing);
+  }
 
-function handleEditclick(){
-    setIsEditing(true)
-}
+  let playerName = <span className="player-name">{name}</span>;
+  //   let btnCaption = "Edit";
 
-let playerName = <span className="player-name">{name}</span>
-let btnCaption ='Edit'
-
-if(isEditing){
-    playerName = <input type="text" requried />
-}
-    return (
-<li>
-         <span className="player">
-         {playerName}
-         <span className="player-symbol">{symbol}</span>
-         </span>
-        <button onClick={handleEditclick}>{btnCaption}</button>
-</li>
-    )
-    
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditclick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
